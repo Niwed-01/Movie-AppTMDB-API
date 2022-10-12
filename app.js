3917e063bc3d68e7658017c233b15814366db7ad
Edwin Manuel Marte Reynoso <66646970+Niwed-01@users.noreply.github.com>
2022-10-12 12:28:21 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,15 +4,128 @@ const API_KEY = 'api_key=b75ff10e9835c3ada8f3c1ac8e976fb8';
 const BASE_Url = 'https://api.themoviedb.org/3';
 const API_Url = BASE_Url + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const Img_Url = 'https://image.tmdb.org/t/p/w500';
-
 const BuscarUrl = BASE_Url + '/search/movie?'+API_KEY;
+
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
 
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const buscar = document.getElementById('buscar');
+const tagsEl = document.getElementById('tags');
 
- 
+var selectedGenre = []
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML= '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id=genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if(selectedGenre.length == 0){
+                selectedGenre.push(genre.id);
+            }else{
+                if(selectedGenre.includes(genre.id)){
+                    selectedGenre.forEach((id, idx) => {
+                        if(id == genre.id){
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            ObtenerPelis(API_Url + '&with_genres='+encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t);
+    })
+}
+
+
+
+
 
 ObtenerPelis(API_Url);
 
@@ -49,15 +162,16 @@ function MostrarPelis(data){
     })
 }
 
-function getColor(vote){
-    if(vote>=8){
+function getColor(vote) {
+    if(vote>= 8){
         return 'green'
-    }else if(vote>=5){
+    }else if(vote >= 5){
         return "orange"
     }else{
         return 'red'
     }
 }
+
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
